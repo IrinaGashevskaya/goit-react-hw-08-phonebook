46f118cdb02beeb26257bec1f2c13966376ec717
IrinaGashevskaya <70062185+IrinaGashevskaya@users.noreply.github.com>
2021-09-10 12:51:26 +0300

    Updates

---
+++ static/js/not-found-view.93bbbc1b.chunk.js
@@ -0,0 +1,2 @@
+(this["webpackJsonpgoit-react-hw-08-phonebook"]=this["webpackJsonpgoit-react-hw-08-phonebook"]||[]).push([[5],{159:function(t,e,n){},178:function(t,e,n){"use strict";n.r(e),n.d(e,"default",(function(){return o}));var r=n.p+"static/media/404-error.88b0d54d.png",c=(n(159),n(3));function o(){return Object(c.jsxs)("main",{role:"alert",className:"",children:[Object(c.jsx)("img",{src:r,width:"650",alt:"Error",className:""}),Object(c.jsx)("h1",{className:"",children:"Page not found "})]})}}}]);
+//# sourceMappingURL=not-found-view.93bbbc1b.chunk.js.map
